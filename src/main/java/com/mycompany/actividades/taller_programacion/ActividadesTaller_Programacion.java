62f0e56687ddbf824746f20a4b30b4cad61557d5
Carlos-Huane <carloshuanesarmiento@gmail.com>
2024-01-09 11:40:46 -0500

    Primer commit1

--- src/main/java/com/mycompany/actividades/taller_programacion/ActividadesTaller_Programacion.java
+++ src/main/java/com/mycompany/actividades/taller_programacion/ActividadesTaller_Programacion.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.actividades.taller_programacion;
 
-/**
- *
- * @author hp
- */
+
 public class ActividadesTaller_Programacion {
 
     public static void main(String[] args) {
