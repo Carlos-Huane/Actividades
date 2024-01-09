d5951d405286f6d0c149386f6a8054d520f5bb1b
Carlos Huane <97753276+Carlos-Huane@users.noreply.github.com>
2024-01-09 12:12:37 -0500

    Delete src/main/java/com/mycompany/actividades/taller_programacion directory

--- src/main/java/com/mycompany/actividades/taller_programacion/ActividadesTaller_Programacion.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.actividades.taller_programacion;
-
-
-public class ActividadesTaller_Programacion {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
